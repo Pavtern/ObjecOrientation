774677de29ccbab9addb901769125515e6b8457c
Pavtern <ternavskii.pavel@gmail.com>
2020-08-16 10:53:22 +0300

    Bread added

--- src/Puppi.java
+++ src/Puppi.java
@@ -1,8 +1,8 @@
 public class Puppi {
-    public Puppi(){
-
-    }
     public  Puppi(String name){
-
+        System.out.println("Name: " + name);
+    }
+    public static void main(String []args){
+        Puppi myPuppi = new Puppi ( "Bread");
     }
 }
